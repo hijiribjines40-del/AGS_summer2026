b10215f5878fbf99490d422cb76a752bebe6403b
AsoAbe <trif1235@icloud.com>
2026-05-19 15:22:44 +0900

    リザルトで獲得コインが上から降ってくる演出

--- Assets/Scenes/GameOver/ResultManager.cs
+++ Assets/Scenes/GameOver/ResultManager.cs
@@ -6,16 +6,20 @@ public class ResultManager : MonoBehaviour
 {
     public TextMeshProUGUI resultText;
 
+    // メダルPrefab複数
+    public GameObject[] medalPrefabs;
+
+    // メダル生成位置
+    public Transform spawnPoint;
+
+    // 落下範囲
+    public float rangeX = 5f;
+
     IEnumerator Start()
     {
-        if (resultText == null)
-        {
-            Debug.LogError("resultText が入っていません");
-            yield break;
-        }
-
         int result = 500;
 
+        // GameManagerがある時
         if (GameManager.Instance != null)
         {
             result = GameManager.Instance.money;
@@ -25,12 +29,39 @@ public class ResultManager : MonoBehaviour
 
         while (count <= result)
         {
+            // 数字更新
             resultText.text =
-                "獲得コイン : " + count;
+                "獲得メダル : " + count;
+
+            // メダル生成
+            CreateMedal();
 
             count++;
 
             yield return new WaitForSeconds(0.01f);
         }
+    }
+
+    void CreateMedal()
+    {
+        Debug.Log(medalPrefabs.Length);
+        // ランダムPrefab
+        int randomIndex =
+            Random.Range(0, medalPrefabs.Length);
+
+        GameObject randomMedal =
+            medalPrefabs[randomIndex];
+
+        // ランダム位置
+        Vector3 pos =
+            spawnPoint.position;
+
+        pos.x += Random.Range(-rangeX, rangeX);
+
+        // 生成
+        Instantiate(
+            randomMedal,
+            pos,
+            Random.rotation);
     }
 }
